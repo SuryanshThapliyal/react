c1dc5bce802c315623cb6547554f4f2f1899d7f9
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-04-02 14:07:04 +0530

    nextproject setup

--- 04currenychanger/src/App.jsx
+++ 04currenychanger/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className=' bg-orange-500 w-full h-20 p-6 rounded-lg'>halo</h1>
+      <h1 className=' bg-orange-500 w-full h-20 p-4.5 rounded-lg text-3xl'>halo</h1>
     </>
   )
 }
