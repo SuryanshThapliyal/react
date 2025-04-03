3bc408bc86a9d5adf68accc20722a407ee971c97
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-04-03 21:04:19 +0530

    currenccy

--- 04currenychanger/src/components/InputBox.jsx
+++ 04currenychanger/src/components/InputBox.jsx
@@ -12,14 +12,15 @@ function InputBox({
     currencyDisable=false,
     className = "",
 }) {
+    const amountId = useId()
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
