68c92da4bd5e900bfdf34504e6d5da06d1efff2d
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2025-06-14 16:52:56 +0530

    capi

--- 07contextapi/src/components/Login.jsx
+++ 07contextapi/src/components/Login.jsx
@@ -13,11 +13,11 @@ function Login() {
     <div>
         <h2>Login</h2>
         <input type='text'
-        // value={username}
+        value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder='username'/>
         <input type='text'
-        // value={password}
+        value={password}
         onChange={(e) => setPassword(e.target.value)}
         placeholder='password'/>
         <button onClick={handleSubmit}>Submit</button>
